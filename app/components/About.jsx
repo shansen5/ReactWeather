8eeb72ebea471687981eb238805f0ea8d47635e2
shansen5 <steven.e.hansen@gmail.com>
2017-05-27 22:25:31 -0700

    Style the about page and link to resources.

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,25 @@
 var React = require( 'react' );
+var {Link} = require( 'react-router' );
 
 var About = ( props ) => {
     return (
-        <div>
-            <h3>About </h3>
-            <p>Welcome to the About Component</p>
+         <div>
+            <h1 className="text-center">About the Weather App</h1>
+            <p>This app shows the weather for cities.</p>
+            <p>
+                A few of the tools that are used:
+            </p>
+            <ul>
+                <li>
+                    <a href='https://github.com/shansen5/ReactWeather'>The GitHub project</a> - The project with all code.
+                </li>
+                <li>
+                    <a href='https://www.udemy.com/the-complete-react-web-app-developer-course/learn/v4/overview'>The Complete React Web App Developer Course</a> - The course that inspired this app.
+                </li>
+                <li>
+                    <a href="http://openweathermap.org">Open Weather Map</a> - The source for the weather data.
+                </li>
+            </ul>
         </div>
     )
 }
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require( 'react' );
+var {Link} = require( 'react-router' );
 
 var Examples = ( props ) => {
     return (
         <div>
-            <h3>Examples</h3>
-            <p>Welcome to the examples page.</p>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are a few example locations to try:</p>
+            <ol>
+                <li>
+                    <Link to='/?city=Bellingham,WA'>Bellingham, WA</Link>
+                </li>
+                <li>
+                    <Link to='/?city=Luxembourg'>Luxembourg</Link>
+                </li>
+            </ol>
         </div>
     )
 }
